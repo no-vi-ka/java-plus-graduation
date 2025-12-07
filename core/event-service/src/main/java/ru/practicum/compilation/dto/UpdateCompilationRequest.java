01d4b077eabd0a2d077ac809990070882ae9648d
Viktoriia Novikova <koncord@yandex.ru>
2025-12-07 21:50:06 +0300

    Set

--- core/event-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
+++ core/event-service/src/main/java/ru/practicum/compilation/dto/UpdateCompilationRequest.java
@@ -5,6 +5,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -15,5 +16,5 @@ public class UpdateCompilationRequest {
     private Boolean pinned;
     @Size(min = 1, max = 50, message = "Length of title must be more than 0 and less than 51.")
     private String title;
-    private List<Long> events;
+    private Set<Long> events;
 }
